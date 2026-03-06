2a8c1c2aaa20304e8acf29382eed565fb307a004
gaston0704 <ioan.gafton@student.upt.ro>
2026-03-06 22:37:44 +0200

    Initial commit: Olympic Gold Medal Statistics REST API

---
+++ src/main/java/com/codecademy/goldmedal/controller/GoldMedalController.java
@@ -0,0 +1,154 @@
+package com.codecademy.goldmedal.controller;
+
+import com.codecademy.goldmedal.repository.*;
+import com.codecademy.goldmedal.model.*;
+import org.apache.commons.text.WordUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/countries")
+public class GoldMedalController {
+    private final GoldMedalRepository goldMedalRepository;
+    private final CountryRepository countryRepository;
+    public GoldMedalController(final  GoldMedalRepository goldMedalRepository, final CountryRepository countryRepository) {
+        this.goldMedalRepository = goldMedalRepository;
+        this.countryRepository = countryRepository;
+    }
+
+    @GetMapping
+    public CountriesResponse getCountries(@RequestParam String sort_by, @RequestParam String ascending) {
+        var ascendingOrder = ascending.toLowerCase().equals("y");
+        return new CountriesResponse(getCountrySummaries(sort_by.toLowerCase(), ascendingOrder));
+    }
+
+    @GetMapping("/{country}")
+    public CountryDetailsResponse getCountryDetails(@PathVariable String country) {
+        String countryName = WordUtils.capitalizeFully(country);
+        return getCountryDetailsResponse(countryName);
+    }
+
+    @GetMapping("/{country}/medals")
+    public CountryMedalsListResponse getCountryMedalsList(@PathVariable String country, @RequestParam String sort_by, @RequestParam String ascending) {
+        String countryName = WordUtils.capitalizeFully(country);
+        var ascendingOrder = ascending.toLowerCase().equals("y");
+        return getCountryMedalsListResponse(countryName, sort_by.toLowerCase(), ascendingOrder);
+    }
+
+    private CountryMedalsListResponse getCountryMedalsListResponse(String countryName, String sortBy, boolean ascendingOrder) {
+        List<GoldMedal> medalsList;
+        switch (sortBy) {
+            case "year":
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByYearAsc(countryName) : goldMedalRepository.getByCountryOrderByYearDesc(countryName);
+                // list of medals sorted by year in the given order
+                break;
+            case "season":
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderBySeasonAsc(countryName) : goldMedalRepository.getByCountryOrderBySeasonDesc(countryName);
+                // list of medals sorted by season in the given order
+                break;
+            case "city":
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByCityAsc(countryName) : goldMedalRepository.getByCountryOrderByCityDesc(countryName);
+                // list of medals sorted by city in the given order
+                break;
+            case "name":
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByNameAsc(countryName) : goldMedalRepository.getByCountryOrderByNameDesc(countryName);
+                // list of medals sorted by athlete's name in the given order
+                break;
+            case "event":
+                medalsList = ascendingOrder ? goldMedalRepository.getByCountryOrderByEventAsc(countryName) : goldMedalRepository.getByCountryOrderByEventDesc(countryName);
+                // list of medals sorted by event in the given order
+                break;
+            default:
+                medalsList = new ArrayList<>();
+                break;
+        }
+
+        return new CountryMedalsListResponse(medalsList);
+    }
+
+    private CountryDetailsResponse getCountryDetailsResponse(String countryName) {
+        var countryOptional = this.countryRepository.getByName(countryName);// get the country
+        if (countryOptional.isEmpty()) {
+            return new CountryDetailsResponse(countryName);
+        }
+
+        var country = countryOptional.get();
+        var goldMedalCount = goldMedalRepository.countByCountry(countryName);// get the medal count
+
+        var summerWins = goldMedalRepository.getByCountryAndSeasonOrderByYearAsc(countryName, "Summer");// get the collection of wins at the Summer Olympics, sorted by year in ascending order
+        var numberSummerWins = summerWins.size() > 0 ? summerWins.size() : null;
+        var totalSummerEvents = goldMedalRepository.countBySeason("Summer");// get the total number of events at the Summer Olympics
+        var percentageTotalSummerWins = totalSummerEvents != 0 && numberSummerWins != null ? (float) summerWins.size() / totalSummerEvents : null;
+        var yearFirstSummerWin = summerWins.size() > 0 ? summerWins.get(0).getYear() : null;
+
+        var winterWins = goldMedalRepository.getByCountryAndSeasonOrderByYearAsc(countryName, "Winter");// get the collection of wins at the Winter Olympics
+        var numberWinterWins = winterWins.size() > 0 ? winterWins.size() : null;
+        var totalWinterEvents = goldMedalRepository.countBySeason("Winter");// get the total number of events at the Winter Olympics, sorted by year in ascending order
+        var percentageTotalWinterWins = totalWinterEvents != 0 && numberWinterWins != null ? (float) winterWins.size() / totalWinterEvents : null;
+        var yearFirstWinterWin = winterWins.size() > 0 ? winterWins.get(0).getYear() : null;
+
+        var numberEventsWonByFemaleAthletes = goldMedalRepository.countByCountryAndGender(countryName, "Women");// get the number of wins by female athletes
+        var numberEventsWonByMaleAthletes = goldMedalRepository.countByCountryAndGender(countryName, "Men");// get the number of wins by male athletes
+
+        return new CountryDetailsResponse(
+                countryName,
+                country.getGdp(),
+                country.getPopulation(),
+                goldMedalCount,
+                numberSummerWins,
+                percentageTotalSummerWins,
+                yearFirstSummerWin,
+                numberWinterWins,
+                percentageTotalWinterWins,
+                yearFirstWinterWin,
+                numberEventsWonByFemaleAthletes,
+                numberEventsWonByMaleAthletes);
+    }
+
+    private List<CountrySummary> getCountrySummaries(String sortBy, boolean ascendingOrder) {
+        List<Country> countries;
+        switch (sortBy) {
+            case "name":
+                countries = ascendingOrder ? countryRepository.getAllByOrderByNameAsc() : countryRepository.getAllByOrderByNameDesc();// list of countries sorted by name in the given order
+                break;
+            case "gdp":
+                countries = ascendingOrder ? countryRepository.getAllByOrderByGdpAsc() : countryRepository.getAllByOrderByGdpDesc();// list of countries sorted by gdp in the given order
+                break;
+            case "population":
+                countries = ascendingOrder ? countryRepository.getAllByOrderByPopulationAsc() : countryRepository.getAllByOrderByPopulationDesc();// list of countries sorted by population in the given order
+                break;
+            case "medals":
+            default:
+                countries = ascendingOrder ? countryRepository.getAllByOrderByPopulationAsc() : countryRepository.getAllByOrderByPopulationDesc();// list of countries in any order you choose; for sorting by medal count, additional logic below will handle that
+                break;
+        }
+
+        var countrySummaries = getCountrySummariesWithMedalCount(countries);
+
+        if (sortBy.equalsIgnoreCase("medals")) {
+            countrySummaries = sortByMedalCount(countrySummaries, ascendingOrder);
+        }
+
+        return countrySummaries;
+    }
+
+    private List<CountrySummary> sortByMedalCount(List<CountrySummary> countrySummaries, boolean ascendingOrder) {
+        return countrySummaries.stream()
+                .sorted((t1, t2) -> ascendingOrder ?
+                        t1.getMedals() - t2.getMedals() :
+                        t2.getMedals() - t1.getMedals())
+                .collect(Collectors.toList());
+    }
+
+    private List<CountrySummary> getCountrySummariesWithMedalCount(List<Country> countries) {
+        List<CountrySummary> countrySummaries = new ArrayList<>();
+        for (var country : countries) {
+            var goldMedalCount = goldMedalRepository.countByCountry(country.getName());// get count of medals for the given country
+            countrySummaries.add(new CountrySummary(country, goldMedalCount));
+        }
+        return countrySummaries;
+    }
+}
